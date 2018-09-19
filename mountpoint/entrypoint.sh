4dd3dbe47dae95627c363719f69b03425c994d24
akihiro <akihiro@kats.la>
2018-09-20 08:11:17 +0900

    ディレクトリ名を変更

--- mountpoint/entrypoint.sh
+++ mountpoint/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/ash -exuC
 
+cd repo
+
 bundle install
 yarn install
+
 bundle exec rails db:create db:migrate
 #bundle exec rails assets:precompile
 bin/webpack
+
 bundle exec rails server -b 0.0.0.0
